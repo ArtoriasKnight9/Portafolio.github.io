0798c329bf4f667d4580e83b74e8141a0220c2b6
ArtoriasKnight9 <maxstell5549@hotmail.com>
2025-04-07 19:49:56 -0600

    javascript

--- Jss/script.js
+++ Jss/script.js
@@ -1,26 +1,14 @@
+let elementoCalif=document.getElementById("calificaciones");
+let resultado=document.getElementById("resultado");
+let status=document.getElementById("status");
 
-const nombre = "Javier";
+FormData.addEventListener("submit", function(event){
+  event.preventDefault();
+  let transactionFormData = new FormData(form); } )
 
-let calificaciones = [100, 90, 80, 70, 60, 80, 60, 90];
-
-let promedio = 0;
-for (let i = 0; i < calificaciones.length; i++) {
-  promedio += calificaciones[i];
-}
-promedio /= calificaciones.length;
-
-console.log("Hola " + nombre + "!");
-console.log("Tus calificaciones son: " + calificaciones);
-console.log("Tu promedio es: " + promedio);
-
-if (promedio <= 50 ) {    
-  console.log("Tienes que recursar la materia");
-} else { (promedio > 50 && promedio < 70)
-  console.log("Te vas a segundas oportunidades");
-} else { (promedio > 70 && promedio < 75); 
-    console.log("Tienes que ir a la recuperacion");
-    } else { (promedio > 75 && promedio < 100); 
-    console.log("Aprobaste la materia");
-    } else { (promedio == 100); 
-    console.log("Felicitaciones, eres un genio");
-    }
+  transactionFormData.getElementById("nombre");
+  transactionFormData.getElementById("apellido");
+  transactionFormData.getElementById("califi1");
+  transactionFormData.getElementById("califi2");
+  transactionFormData.getElementById("califi3");
+  transactionFormData.getElementById("concentrado");
