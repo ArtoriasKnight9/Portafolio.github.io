57c4e745ef5b9eeb5a97360938beeaf8afe8649d
ArtoriasKnight9 <maxstell5549@hotmail.com>
2025-04-09 01:54:50 -0600

    Jss Final

--- Jss/script.js
+++ Jss/script.js
@@ -1,6 +1,12 @@
 const form = document.getElementById("transactionForm");
 var inputlimpio = "";
 
+document.getElementById("concentradoRef").addEventListener("click", function(e) {
+  if(e.target.classList.contains("btn-eliminar")) {
+    eliminarFila(e); // Reutilizamos la misma función
+  }
+});
+
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // Evita el envío del formulario por defecto 
 
@@ -24,18 +30,21 @@ form.addEventListener("submit", function(event) {
   newtypeCellref = newTransactionRowRef.insertCell(4);
   newtypeCellref.textContent = transactionFormData.get("calificacion3");
   
-  newtypeCellref = newTransactionRowRef.
+  newtypeCellref = newTransactionRowRef.insertCell(5);
+  newtypeCellref.textContent = calcularpromedio(transactionFormData); // Llama a la función y pasa el FormData
 
   // Llama a la función y pasa el FormData
   newtypeCellref.textContent = calcularpromedio(transactionFormData);
+
+  agregarBotonEliminar(newTransactionRowRef); // Agrega el botón de eliminar a la nueva fila
 
   form.reset(); // Limpia el formulario después de agregar la fila
   
   colorerfila()
   document.getElementById(promedioGeneral) 
   calcularpromediogeneral();
-  
  
+  
 
 });
 
@@ -96,4 +105,36 @@ function colorerfila() {
   }
 }
 
+function agregarBotonEliminar(fila) {
+  // Crear celda para el botón
+  const celdaBoton = fila.insertCell(-1); // -1 para insertar al final
+  
+  // Crear el botón
+  const botonEliminar = document.createElement('button');
+  botonEliminar.textContent = 'Eliminar';
+  botonEliminar.className = 'btn-eliminar';
+  
+  // Asignar evento
+  botonEliminar.addEventListener('click', eliminarFila);
 
+  botonEliminar.className = `
+  bg-yellow-300 hover:bg-red-500 font-medium
+  py-1 px-3 rounded-md 
+`;
+  
+  // Agregar botón a la celda
+  celdaBoton.appendChild(botonEliminar);
+}
+
+function eliminarFila(event) {
+  // Obtener la fila (tr) padre del botón clickeado
+  const fila = event.target.closest('tr');
+  
+  
+    fila.remove();
+    // Recalcular el promedio general después de eliminar
+    calcularpromediogeneral();
+    // Reaplicar colores si es necesario
+    colorerfila();
+  
+}
